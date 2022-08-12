2df47365b117285c93ce21fe994ab375500d1a16
Daniel Murphy <knysnacoder@gmail.com>
2022-08-12 10:49:49 +0200

    Setting up email and adding attach_file attribute

--- playground/views.py
+++ playground/views.py
@@ -1,10 +1,12 @@
-from django.core.mail import send_mail, mail_admins, BadHeaderError
+from django.core.mail import EmailMessage, BadHeaderError
 from django.shortcuts import render
 
 # this auto sends an email once the end point is hit /playground/hello/
 def say_hello(request):
     try:
-        mail_admins('message', 'subject', html_message='message')
+        message = EmailMessage('message', 'subject', 'info@cws.com', ['bob@cws.com'])
+        message.attach_file('playground/static/images/20210526_152956(0).jpg')
+        message.send()
     except BadHeaderError:
         pass
     return render(request, 'hello.html', {'name': 'Mosh'})
