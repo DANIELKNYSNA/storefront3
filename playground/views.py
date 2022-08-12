96bd566e4c4d81a4085b74b704791a878d97017f
Daniel Murphy <knysnacoder@gmail.com>
2022-08-12 11:40:03 +0200

    Setting up a base template for emails

--- playground/views.py
+++ playground/views.py
@@ -1,12 +1,15 @@
 from django.core.mail import EmailMessage, BadHeaderError
 from django.shortcuts import render
+from templated_mail.mail import BaseEmailMessage
 
 # this auto sends an email once the end point is hit /playground/hello/
 def say_hello(request):
     try:
-        message = EmailMessage('message', 'subject', 'info@cws.com', ['bob@cws.com'])
-        message.attach_file('playground/static/images/20210526_152956(0).jpg')
-        message.send()
+        message = BaseEmailMessage(
+            template_name='emails/hello.html', 
+            context={'name': 'Daniel'}
+        )
+        message.send(['bob@cws.com'])
     except BadHeaderError:
         pass
-    return render(request, 'hello.html', {'name': 'Mosh'})
+    return render(request, 'hello.html', {'name': 'Daniel'})
