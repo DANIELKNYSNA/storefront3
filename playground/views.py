88c21d28cfd1c4e5b2c9aacc61ba475c149cf3d6
Daniel Murphy <knysnacoder@gmail.com>
2022-08-12 10:29:09 +0200

    Sending an email to a specific recipient

--- playground/views.py
+++ playground/views.py
@@ -1,5 +1,10 @@
+from django.core.mail import send_mail, mail_admins, BadHeaderError
 from django.shortcuts import render
 
-
+# this auto sends an email once the end point is hit /playground/hello/
 def say_hello(request):
+    try:
+        send_mail('subject', 'message', 'info@cws.com', ['bob@cws.com'])
+    except BadHeaderError:
+        pass
     return render(request, 'hello.html', {'name': 'Mosh'})
