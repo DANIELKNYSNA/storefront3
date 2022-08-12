d582554bd2f849c7d04aee0670ce97ec29dec82f
Daniel Murphy <knysnacoder@gmail.com>
2022-08-12 10:37:26 +0200

    Sending admin emails and configuring ADMINS

--- playground/views.py
+++ playground/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import render
 # this auto sends an email once the end point is hit /playground/hello/
 def say_hello(request):
     try:
-        send_mail('subject', 'message', 'info@cws.com', ['bob@cws.com'])
+        mail_admins('message', 'subject', html_message='message')
     except BadHeaderError:
         pass
     return render(request, 'hello.html', {'name': 'Mosh'})
--- storefront/settings.py
+++ storefront/settings.py
@@ -181,3 +181,7 @@ EMAIL_HOST_USER = ''
 EMAIL_HOST_PASSWORD = ''
 EMAIL_PORT = 2525
 DEFAULT_FROM_EMAIL = 'whatever email you want'
+
+ADMINS = [
+    ('Daniel', 'admin@cws.com'),
+]
