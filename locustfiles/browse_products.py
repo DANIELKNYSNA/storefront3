45d5df210046b185841f0f09c6e9d4e316055a78
Daniel Murphy <knysnacoder@gmail.com>
2022-08-14 16:44:30 +0200

    Updating a locust function

--- locustfiles/browse_products.py
+++ locustfiles/browse_products.py
@@ -1,36 +1,40 @@
 from locust import HttpUser, task, between
 from random import randint
 
+
 class WebsiteUser(HttpUser):
-  wait_time = between(1, 5)  
-  
-  # Viewing products
-  @task(2)
-  def view_products(self):
-    print('View Products')
-    collection_id = randint(2, 6)
-    self.client.get(f'/store/products/?collection_id={collection_id}', name='/store/products')
-  # Viewing product details
-  @task(4)
-  def view_products(self):
-    print('View Product Details')
-    product_id = randint(1, 1000)
-    self.client.get(f'/store/products/{product_id}/', name='/store.products/:id')
-  # Add product to cart
-  @task(1)
-  def add_to_cart(self):
-    print('Add to cart')
-    product_id = randint(1, 10)
-    self.client.post(
-      f'/store/carts/{self.cart_id}/items/', 
-      name='/store/carts/items', 
-      json={'product_id': product_id, 'quantity': 1}
-    )
+    wait_time = between(1, 5)
 
-  # this is a life cycle hook which starts as soon as a client logs on
-  # We need this to generate a cart_id
-  def on_start(self):
-    response = self.client.post('/store/carts/')
-    result = response.json()
-    self.cart_id = result['id']
+    # Viewing products
+    @task(2)
+    def view_products(self):
+        print('View Products')
+        collection_id = randint(2, 6)
+        self.client.get(
+            f'/store/products/?collection_id={collection_id}', name='/store/products')
+    # Viewing product details
 
+    @task(4)
+    def view_product(self):
+        print('View Product Details')
+        product_id = randint(1, 1000)
+        self.client.get(
+            f'/store/products/{product_id}/', name='/store.products/:id')
+    # Add product to cart
+
+    @task(1)
+    def add_to_cart(self):
+        print('Add to cart')
+        product_id = randint(1, 10)
+        self.client.post(
+            f'/store/carts/{self.cart_id}/items/',
+            name='/store/carts/items',
+            json={'product_id': product_id, 'quantity': 1}
+        )
+
+    # this is a life cycle hook which starts as soon as a client logs on
+    # We need this to generate a cart_id
+    def on_start(self):
+        response = self.client.post('/store/carts/')
+        result = response.json()
+        self.cart_id = result['id']
