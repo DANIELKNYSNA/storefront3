ae4b9a58adaf13cff5a4a8b85455f6e6ea27afe7
Daniel Murphy <knysnacoder@gmail.com>
2022-08-15 14:59:51 +0200

    Provisioning the MYSQL database using dj_database_url

--- storefront/settings/prod.py
+++ storefront/settings/prod.py
@@ -2,9 +2,14 @@ import os
 
 from .dev import SECRET_KEY
 from .common import *
+import dj_database_url
 
 DEBUG = False
 
 SECRET_KEY = os.environ['SECRET_KEY']
 
 ALLOWED_HOSTS = ["dan-prod.herokuapp.com"]
+
+DATABASES = {
+    'default': dj_database_url.config()
+}
