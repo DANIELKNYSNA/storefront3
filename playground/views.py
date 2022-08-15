5588c9cda3e43b284a0ce0b0d190526a8e98ad09
Daniel Murphy <knysnacoder@gmail.com>
2022-08-15 12:33:22 +0200

    Adding a logger to playground.views

--- playground/views.py
+++ playground/views.py
@@ -1,16 +1,23 @@
-from django.core.cache import cache
 from django.shortcuts import render
 from django.utils.decorators import method_decorator
 from django.views.decorators.cache import cache_page
 from rest_framework.views import APIView
+import logging
 import requests
+
+logger = logging.getLogger(__name__)  # this translates to playground.views
 
 
 class HelloView(APIView):
-    @method_decorator(cache_page(5*60))
+    # @method_decorator(cache_page(5*60))
     def get(self, request):
-        response = requests.get('https://httpbin.org/delay/2')
-        data = response.json()
+        try:
+            logger.info('Calling httpbin')
+            response = requests.get('https://httpbin.org/delay/2')
+            logger.info('Received the response')
+            data = response.json()
+        except requests.ConnectionError:
+            logger.critical('httpbin is offline')
         return render(request, 'hello.html', {'name': data})
 
 
