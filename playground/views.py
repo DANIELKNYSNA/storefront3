8bd928310903b16fa80da0b33da93494f189829c
Daniel Murphy <knysnacoder@gmail.com>
2022-08-15 09:45:41 +0200

    Setting up caching using class based views

--- playground/views.py
+++ playground/views.py
@@ -1,13 +1,20 @@
 from django.core.cache import cache
 from django.shortcuts import render
+from django.utils.decorators import method_decorator
+from django.views.decorators.cache import cache_page
+from rest_framework.views import APIView
 import requests
 
 
-def say_hello(request):
-    # this gives us a delay of 2 seconds at this endpoint, simulating a slow api call
-    key = 'httpbin_result'
-    if cache.get(key) is None:
+class HelloView(APIView):
+    @method_decorator(cache_page(5*60))
+    def get(self, request):
         response = requests.get('https://httpbin.org/delay/2')
         data = response.json()
-        cache.set(key, data, 10 * 60 )
-    return render(request, 'hello.html', {'name': cache.get(key)})
+        return render(request, 'hello.html', {'name': data})
+
+
+@cache_page(5 * 60)
+def say_hello(request):
+    pass
+    # this gives us a delay of 2 seconds at this endpoint, simulating a slow api call
